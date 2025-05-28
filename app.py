700d2fdae61086d99d888cdad4da07c5fa606c09
gbilbrey <george.bilbrey@gmail.com>
2025-05-28 16:31:11 -0600

    Fix Flask app for production: remove debug mode, use PORT env var

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -142,4 +143,5 @@ def api_advisory_submit():
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
